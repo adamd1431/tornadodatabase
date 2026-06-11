71d274778b2693e585f5e7c6530b563625afbccf
Adam <lazerbuffalo1431@gmail.com>
2026-06-10 19:13:21 -0600

    fix supabase key

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabaseUrl = "https://kxhqfwlfendphhwurhcg.supabase.co";
-const supabaseKey = "sb_publishable_CBVEIcLFuscGmMksil5gWQ_vpXUFLOm";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4aHFmd2xmZW5kcGhod3VyaGNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NDI4MjQsImV4cCI6MjA5MzQxODgyNH0.K4kwmU_i9qln-74xGX5Qj-VqwsJIlERvF3VAMXUcMa4";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
